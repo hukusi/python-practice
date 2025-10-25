2c09b2ce3bbbd4fd79d4e6997cbd4a15317354c1
ryo-666 <r.otias.0113@gmail.com>
2025-10-25 20:43:44 +0900

    基礎

--- app_code.py
+++ app_code.py
@@ -1,4 +1,38 @@
-a = 1
-b = 2
-print(a + b)
+# a = 1
+# b = 2
+# print(a + b)
+# print(a + b)
+# print(a)
+ 
+'''
+  print(a + b)
+  print(a + b)
+'''
 
+# x = 'バナナ'
+
+# print(x)
+
+# x = 'トマト'
+
+# print(x)
+
+# z = ['a', 'b', 'c', 100]
+# x = ['e', 'f']
+
+# print(z)
+# print(len(z))
+# print(z[1])
+
+# z.append('e')
+# z.remove('b')
+
+# print(z)
+
+# z.extend(x)
+
+# print(z)
+
+# z = ['a', 'b', 'c', 100]
+
+# print(z[1:3])
